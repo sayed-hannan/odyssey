2e6707f74ea51e388684372b530a7aef43051852
sayed-hannan <110350353+sayed-hannan@users.noreply.github.com>
2023-09-29 21:21:21 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to My Website</h1>
+      <h1>Welcome to My Website Huck off!</h1>
       this si my fjudsakj 
       <p>;lhlíh'lih'li</p>
       {/* Your content goes here */}
